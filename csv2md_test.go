b17af9032f8f8b482ee42983ab52e3a17e5b377f
Frido Koch <info@fridokoch.de>
2015-08-01 10:20:41 +0200

    Tests

--- csv2md_test.go
+++ csv2md_test.go
@@ -3,9 +3,24 @@ package main
 import (
 	"bytes"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func BenchmarkMaxLength(b *testing.B) {
+func TestMaxColLen(t *testing.T) {
+	in := [][]string{
+		{"123", "sdfgerwgeg", "sdf43terge", "sad", "tzutzutz"},
+		{"hfghfgh", "3", "fghehjthjgfhjezthg", "sad", "tzutzutzttttttttttt"},
+		{"545", "4554", "hgfhfg", "sad", "ghjtzj65hgfh"},
+		{"fghfghfg", "fghfg", "tuzutzutzt", "hgjez565ztd", "tzutzutz"},
+	}
+	ex := []int{8, 10, 18, 11, 19}
+	ac, err := maxColLen(in)
+	assert.Nil(t, err)
+	assert.Equal(t, ex, ac, "Result ist wrong")
+}
+
+func BenchmarkMaxColLen(b *testing.B) {
 	a := [][]string{
 		{"123", "sdfgerwgeg", "sdf43terge", "sad", "tzutzutz"},
 		{"hfghfgh", "3", "fghehjthjgfhjezthg", "sad", "tzutzutzttttttttttt"},
